7c207cdbbd7856da4749e328177508692a92358d
hahwul <hahwul@gmail.com>
2021-02-02 00:46:04 +0900

    update

---
+++ tools/subs.sh
@@ -0,0 +1,10 @@
+# Pull targets
+wget https://raw.githubusercontent.com/KingOfBugbounty/KingOfBugBountyTips/master/downlink ; xargs -a downlink -I@ sh -c 'wget @ -q'; mkdir bounty ; unzip -o '*.zip' -d bounty/ ; rm -rf *zip ; cat bounty/*.txt | sort -u >> ./docs/subs.txt
+
+# Calc subs
+wc -l docs/subs.txt | cut -d " " -f 1 | tee ./docs/line-subs.txt
+ls -alh docs/subs.txt | cut -d " " -f 5 | tee ./docs/size-subs.txt
+
+
+# Make http
+#httpx -l docs/subs.txt -title -web-server -threads 500 -status-code -o docs/http.txt
