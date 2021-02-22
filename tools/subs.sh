b6d762edd173439330c741bd01a04aa3afad2e5b
HAHWUL <hahwul@gmail.com>
2021-02-23 00:30:35 +0900

    Update subs.sh

--- tools/subs.sh
+++ tools/subs.sh
@@ -2,6 +2,5 @@
 wget https://raw.githubusercontent.com/KingOfBugbounty/KingOfBugBountyTips/master/downlink ; xargs -a downlink -I@ sh -c 'wget @ -q'; mkdir bounty ; unzip -o '*.zip' -d bounty/ ; rm -rf *zip ; cat bounty/*.txt | sort -u > ./docs/subs.txt
 
 # Calc subs
-wc -l docs/subs.txt | cut -d " " -f 1 | tee ./docs/line-subs.txt
-ls -alh docs/subs.txt | cut -d " " -f 5 | tee ./docs/size-subs.txt
-
+wc -l docs/subs.txt | cut -d " " -f 1 | ~/go/bin/gee ./docs/line-subs.txt
+ls -alh docs/subs.txt | cut -d " " -f 5 | ~/go/bin/gee ./docs/size-subs.txt
