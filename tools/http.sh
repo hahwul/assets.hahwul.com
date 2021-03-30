323f67c363e951eb7dd2e08889da791fc8e37f21
hahwul <hahwul@gmail.com>
2021-03-30 17:40:36 +0900

    update

--- tools/http.sh
+++ tools/http.sh
@@ -1,6 +1,10 @@
 # Make http
-~/go/bin/httpx -l docs/subs.txt -threads 500 -status-code -o docs/http.txt
+#~/go/bin/httpx -l docs/subs.txt -threads 500 -status-code -o docs/http.txt
+
+# wc -l docs/http.txt | cut -d " " -f 1 | tee ./docs/line-http.txt
+# ls -alh docs/http.txt | cut -d " " -f 5 | tee ./docs/size-http.txt
 
 # Calc http
-wc -l docs/http.txt | cut -d " " -f 1 | tee ./docs/line-http.txt
-ls -alh docs/http.txt | cut -d " " -f 5 | tee ./docs/size-http.txt
+LINE=`wc -l docs/http.txt | cut -d " " -f 1`
+SIZE=`ls -alh docs/http.txt | cut -d " " -f 5`
+echo \{\"path\":\"http.txt\",\"line\":\"$LINE\",\"size\":\"$SIZE\"\} > docs/http.json
