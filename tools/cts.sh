de441d90f53f222d153706c23f498ff3cd2edd6c
hahwul <hahwul@gmail.com>
2021-04-03 22:46:46 +0900

    update

---
+++ tools/cts.sh
@@ -0,0 +1,29 @@
+# Pull domains
+curl https://raw.githubusercontent.com/internetwache/CT_subdomains/master/top-100.txt | grep -v "\#" | grep -v "count,subdomain" | cut -d "," -f 2 | tee ./docs/cts-100.txt
+
+curl https://raw.githubusercontent.com/internetwache/CT_subdomains/master/top-1000.txt | grep -v "\#" | grep -v "count,subdomain" | cut -d "," -f 2 | tee ./docs/cts-1000.txt
+
+curl https://raw.githubusercontent.com/internetwache/CT_subdomains/master/top-10000.txt | grep -v "\#" | grep -v "count,subdomain" | cut -d "," -f 2 | tee ./docs/cts-10000.txt
+
+curl https://raw.githubusercontent.com/internetwache/CT_subdomains/master/top-100000.txt | grep -v "\#" | grep -v "count,subdomain" | cut -d "," -f 2 | tee ./docs/cts-100000.txt
+
+# Calc subs
+LINE=`wc -l docs/cts-100.txt | cut -d " " -f 1`
+SIZE=`ls -alh docs/cts-100.txt | cut -d " " -f 5`
+LDATE=`date -R`
+echo \{\"path\":\"cts-100.txt\",\"line\":\"$LINE\",\"size\":\"$SIZE\",\"date\":\"$LDATE\"\} > docs/cts-100.json
+
+LINE=`wc -l docs/cts-1000.txt | cut -d " " -f 1`
+SIZE=`ls -alh docs/cts-1000.txt | cut -d " " -f 5`
+LDATE=`date -R`
+echo \{\"path\":\"cts-1000.txt\",\"line\":\"$LINE\",\"size\":\"$SIZE\",\"date\":\"$LDATE\"\} > docs/cts-1000.json
+
+LINE=`wc -l docs/cts-10000.txt | cut -d " " -f 1`
+SIZE=`ls -alh docs/cts-10000.txt | cut -d " " -f 5`
+LDATE=`date -R`
+echo \{\"path\":\"cts-10000.txt\",\"line\":\"$LINE\",\"size\":\"$SIZE\",\"date\":\"$LDATE\"\} > docs/cts-10000.json
+
+LINE=`wc -l docs/cts-100000.txt | cut -d " " -f 1`
+SIZE=`ls -alh docs/cts-100000.txt | cut -d " " -f 5`
+LDATE=`date -R`
+echo \{\"path\":\"cts-100000.txt\",\"line\":\"$LINE\",\"size\":\"$SIZE\",\"date\":\"$LDATE\"\} > docs/cts-100000.json
