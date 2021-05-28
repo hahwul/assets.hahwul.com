e1754d2f904624f4e6456e4a01e510e9744ecd91
HAHWUL <hahwul@gmail.com>
2021-05-29 02:47:47 +0900

    Create wl-payloadbox-xss.sh

---
+++ tools/wl-payloadbox-xss.sh
@@ -0,0 +1,7 @@
+wget https://raw.githubusercontent.com/payloadbox/xss-payload-list/master/Intruder/xss-payload-list.txt -o docs/wl-payloadbox-xss.txt
+
+# Calc subs
+LINE=`wc -l docs/wl-payloadbox-xss.txt | cut -d " " -f 1`
+SIZE=`ls -alh docs/wl-payloadbox-xss.txt | cut -d " " -f 5`
+LDATE=`date -R`
+echo \{\"path\":\"wl-payloadbox-xss.txt.txt\",\"line\":\"$LINE\",\"size\":\"$SIZE\",\"date\":\"$LDATE\"\} > docs/wl-payloadbox-xss.json
