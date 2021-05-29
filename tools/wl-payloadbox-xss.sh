94fb51914e4ed898e8ac6c309e09d0b1c0deff88
hahwul <hahwul@gmail.com>
2021-05-29 18:49:09 +0900

    update
    Signed-off-by: hahwul <hahwul@gmail.com>

--- tools/wl-payloadbox-xss.sh
+++
@@ -1,7 +0,0 @@
-wget https://raw.githubusercontent.com/payloadbox/xss-payload-list/master/Intruder/xss-payload-list.txt -O docs/wl-payloadbox-xss.txt
-
-# Calc subs
-LINE=`wc -l docs/wl-payloadbox-xss.txt | cut -d " " -f 1`
-SIZE=`ls -alh docs/wl-payloadbox-xss.txt | cut -d " " -f 5`
-LDATE=`date -R`
-echo \{\"path\":\"wl-payloadbox-xss.txt.txt\",\"line\":\"$LINE\",\"size\":\"$SIZE\",\"date\":\"$LDATE\"\} > docs/wl-payloadbox-xss.json
