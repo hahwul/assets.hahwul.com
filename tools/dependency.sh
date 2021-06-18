157a6ebe66e6da2300cc9d8cc0ba8374da9c90f6
HAHWUL <hahwul@gmail.com>
2021-06-18 18:58:50 +0900

    Update dependency.sh

--- tools/dependency.sh
+++ tools/dependency.sh
@@ -1,3 +1,4 @@
 GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
 GO111MODULE=on go get -v github.com/hahwul/gee
 GO111MODULE=on go get -v github.com/tomnomnom/gron
+apt install util-linux
