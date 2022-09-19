e729519ed6a7522ffea8cf020e40b99e9e2c9d20
hahwul <hahwul@gmail.com>
2022-09-19 23:56:25 +0900

    Add nuclei-latest templates
    Signed-off-by: hahwul <hahwul@gmail.com>

---
+++ tools/nuclei-latest.rb
@@ -0,0 +1,21 @@
+require "open-uri"
+require "json"
+
+@data = URI.parse("https://api.github.com/repos/projectdiscovery/nuclei-templates/releases/latest").read
+jobject = JSON.parse @data
+body = jobject['body']
+arr = body.split("\r\n")
+
+
+result = {
+  "updated": Time.now,
+  "templates":[]
+}
+arr.each do |line|
+  if line.include? ' by '
+    splited_line = line.gsub("- ","").split(' ')
+    result[:templates].push "#{splited_line[0]}"
+  end
+end
+
+File.write("./docs/nuclei-latest.json",result.to_json)
